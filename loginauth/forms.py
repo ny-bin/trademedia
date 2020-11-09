5b4481935d755fc4aec5b21cb654fe57a9aa893a
ny_uta <39217395+yuuuuto1210@users.noreply.github.com>
2020-11-09 23:44:26 +0900

    finish create user

--- loginauth/forms.py
+++ loginauth/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
-from . import User
+from .models import User
 
 class LoginForm(AuthenticationForm):
     """ログオンフォーム"""
@@ -15,9 +15,10 @@ class UserCreateForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         #htmlの表示を変更可能にします
         self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['password'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['class'] = 'form-control'
 
     class Meta:
        model = User
-       fields = ("username", "password", "email",)
+       fields = ("username", "password1","password2", "email",)
--- loginauth/views.py
+++ loginauth/views.py
@@ -3,9 +3,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import (LoginView, LogoutView)
 from django.contrib.auth import login, authenticate
 from django.views.generic import CreateView
-from .forms import LoginForm
-from .forms import UserCreateForm
+from loginauth.forms import LoginForm
 from .models import User
+from .forms import UserCreateForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -19,22 +19,24 @@ class Logout(LoginRequiredMixin, LogoutView):
     """ログアウトページ"""
     template_name = 'loginauth/login.html'
 
-class Create_user(CreateView):
-    def post(self, request, *args, **kwargs):
-        form = UserCreateForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            #フォームから'username'を読み取る
-            username = form.cleaned_data.get('user_name')
-            #フォームから'password1'を読み取る
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            login(request, User)
-            return redirect('/')
-        return render(request, 'create_user.html', {'form': form,})
-
-    def get(self, request, *args, **kwargs):
+def signup(request):
+    if request.method == 'POST':
+        # user.username = request.POST["user_name"]
         form = UserCreateForm(request.POST)
-        return  render(request, 'create_user.html', {'form': form,})
+        print(request.POST["username"])
+        # form.password1 = request.POST["password1"]
+        # form.password2 = request.POST["password2"]
+        if form.is_valid():
+            print("検証に成功しました。データを保存します")
+            form.save()
+        else:
+            print("検証に失敗したので、データを保存しません。検証に失敗した理由を次に表示します。")
+            print(form.errors)
+        # user = form.save()
+        # login(request, user)
+        # return redirect('/login')
+    # else:
+    #     form = UserCreationForm(User)
+    return render(request, 'loginauth/create_user.html')
 
 
