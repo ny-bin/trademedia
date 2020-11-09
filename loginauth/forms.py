2b4d8cb28a3434b81b79d56d24b18729bdc66e67
nakai <nakai@ibis.gs>
2020-11-09 14:52:39 +0900

    create user form

--- loginauth/forms.py
+++ loginauth/forms.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import AuthenticationForm 
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from . import User
 
 class LoginForm(AuthenticationForm):
     """ログオンフォーム"""
@@ -7,4 +8,16 @@ class LoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['placeholder'] = field.label   
+            field.widget.attrs['placeholder'] = field.label
+            
+class UserCreateForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #htmlの表示を変更可能にします
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+
+    class Meta:
+       model = User
+       fields = ("username", "password", "email",)
--- loginauth/views.py
+++ loginauth/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-
-# ここから追加
+from django.shortcuts import render,redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import(LoginView, LogoutView)
+from django.contrib.auth.views import (LoginView, LogoutView)
+from django.contrib.auth import login, authenticate
+from django.views.generic import CreateView
 from .forms import LoginForm
+from .forms import UserCreateForm
+from .models import User
 
 # Create your views here.
 from django.http import HttpResponse
@@ -16,7 +18,23 @@ class Login(LoginView):
 class Logout(LoginRequiredMixin, LogoutView):
     """ログアウトページ"""
     template_name = 'loginauth/login.html'
-# ここまで追加
 
-class Create_User():
-    pass
+class Create_user(CreateView):
+    def post(self, request, *args, **kwargs):
+        form = UserCreateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            #フォームから'username'を読み取る
+            username = form.cleaned_data.get('user_name')
+            #フォームから'password1'を読み取る
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, User)
+            return redirect('/')
+        return render(request, 'create_user.html', {'form': form,})
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreateForm(request.POST)
+        return  render(request, 'create_user.html', {'form': form,})
+
+
